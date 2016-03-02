7f7405ab5a0e723a29abc58e0865f564ec8315fd
r0cco <Rocco.Riccardi.42@gmail.com>
2016-03-01 23:40:37 -0500

    added sln to repo. Started to write the program, very barebones

---
+++ Connect4.BLL/GameModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4.BLL
+{
+    public class GameModel
+    {
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public Board GameBoard { get; set; }
+    }
+}
---
+++ Connect4/GameFlow.cs
@@ -0,0 +1,38 @@
+﻿using Connect4.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4
+{
+    public class GameFlow
+    {
+        private GameModel _model = new GameModel();
+
+        public void PlayGame()
+        {
+            GetPlayersNames();
+        }
+
+        private void GetPlayersNames()
+        {
+            Console.Write("Player 1, What is your name? : ");
+            _model.Player1Name = Console.ReadLine();
+            if (_model.Player1Name == String.Empty)
+            {
+                _model.Player1Name = "Player1";
+            }
+
+            Console.Write("\n\nPlayer 2, what is your name? : ");
+            _model.Player2Name = Console.ReadLine();
+            if (_model.Player2Name == String.Empty)
+            {
+                _model.Player2Name = "Player2";
+            }
+
+            Console.Clear();
+        }
+    }
+}
