6e3d3ebb56c298c00bf0ab5e4f826a59649be0f1
r0cco <Rocco.Riccardi.42@gmail.com>
2016-03-02 23:54:53 -0500

    started writing gameflow methods for taking turns. In the middle of it, basically.

--- Connect4/GameFlow.cs
+++ Connect4/GameFlow.cs
@@ -19,11 +19,14 @@ namespace Connect4
         {
             _model.GameBoard = new Board();
 
-            GetPlayersNames();
-            BoardUI.DisplayGameBoard(_model.GameBoard);
+            if (_freshGame)
+            {
+                GetPlayerNames();
+            }
+            PlaceGamePieces();
         }
 
-        private void GetPlayersNames()
+        private void GetPlayerNames()
         {
             Console.Write("Player 1, What is your name? : ");
             _model.Player1Name = Console.ReadLine();
@@ -40,6 +43,51 @@ namespace Connect4
             }
 
             Console.Clear();
+        }
+
+        private void PlaceGamePieces()
+        {
+            while (!_gameOver)
+            {
+                while (_isPlayerOnesTurn && !_gameOver)
+                {
+                    TakeTurn(_model.Player1Name, _model.GameBoard, _model.Player2Name);
+                }
+                while (!_isPlayerOnesTurn && !_gameOver)
+                {
+                    TakeTurn(_model.Player2Name, _model.GameBoard, _model.Player1Name);
+                }
+            }
+        }
+
+        private void TakeTurn(string currentPlayerName, Board board, string opposingPlayerName)
+        {
+            string columnInput;
+
+            bool inputIsValid;
+            do
+            {
+                BoardUI.DisplayGameBoard(board);
+
+                Console.WriteLine();
+                Console.Write("{0}, select a column : ", currentPlayerName);
+                columnInput = Console.ReadLine();
+                //TODO method to validate columnInput and set input to valid
+                inputIsValid = true;
+
+                if (!inputIsValid)
+                {
+                    Console.WriteLine("That is not a valid column!");
+                    Console.WriteLine("Press enter to try again");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            } while (!inputIsValid);
+
+            var column = int.Parse(columnInput);
+            var response = board.PlaceGamePiece(column, true); //TODO fix this to input a players turn
+
+            //TODO finish writing this method
         }
     }
 }
